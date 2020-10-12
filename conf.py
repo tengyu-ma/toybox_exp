d44932271f7e08698f54844735a1efd6809078ba
tengyum <tengyu.ma@vanderbilt.edu>
2020-10-11 21:28:21 -0500

    update folder for colab

--- conf.py
+++ conf.py
@@ -52,7 +52,6 @@ ToyboxDataDirs = {
     # 'ENG-AIVASLAB1': '/home/mat/Data/v2/ModelNet/ModelNet40/DSCDSC/SOFT_C16384',  # My Lab 1060, 10.20.141.40
     # 'VUSE-10397': '/home/mat/Data/v2/ModelNet/ModelNet40/DSCDSC/SOFT_C16384',  # Ryan Lab 1060, 10.20.141.186
     'tengyu-ubuntu': '/media/tengyu/DataU/Data/Toybox/squares_rz',  # Home
-    'd2f80b39a68e': '/content/drive/My Drive/data/squares_rz'  # Colab
 }
 
 ToyboxLogDirs = {
@@ -60,8 +59,8 @@ ToyboxLogDirs = {
     # 'ENG-AIVASLAB1': '/home/mat/Log/V2Exp',  # My Lab 1060, 10.20.141.40
     # 'VUSE-10397': '/home/mat/Log/V2Exp',  # Ryan Lab 1060, 10.20.141.186
     'tengyu-ubuntu': '/media/tengyu/DataU/Log/ToyboxExp',  # Home
-    'd2f80b39a68e': '/content/drive/My Drive/log/ToyboxExp'  # Colab
 }
 
-ToyboxDataDir = ToyboxDataDirs[HostName]
-ToyboxLogDir = ToyboxLogDirs[HostName]
+ToyboxDataDir = ToyboxDataDirs.get(HostName, '/content/drive/My Drive/data/squares_rz')  # Colab
+ToyboxLogDir = ToyboxLogDirs.get(HostName, '/content/drive/My Drive/log/ToyboxExp')  # Default Colab
+
